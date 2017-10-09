86abce68b5792fe22e438ffaa0241a2a4b736d4c
Greg Schultz <gjs86@cornell.edu>
2017-10-08 23:41:29 -0400

    docstrings

--- individual_validate.py
+++ individual_validate.py
@@ -10,41 +10,59 @@ import requests
 from colors import bcolors
 
 '''
-    Designed for use on non-empty, individual directories to evaluate the .html and .php files on the root directory. 
+    Designed for validating a non-empty, individual directory, and (possibly) with a .zip file containing the student's website.
     WARNING there is no caching, so you could get 403'd quickly if you run this validator repeatedly, use sparingly. 
+
+    self.
+        show_output (Boolean:False) : Show Validation output for each file
+        error_descriptions (List) : 
+        err_files (Dict) : Tracks which files have errors for this student 
+                      ex :  {self.netID : {'err_file1.html', 'err_file2.html} }
+        root (String) : Absolute path to the CMS output directory
+        html_validator_url (String) : w3 validation service endpoint
+        headers (JSON) : Headers for w3 validation
+        files_to_validate (List[String]) : .html or .php files that need to be validated for this student
+        netID (String) : Student's netID
 '''
 
 class Individual_Validate():
-    def __init__(self, root_path, displayOutput=False, unzip=True):
-        self.show_output = displayOutput
+    def __init__(self, root_path, display_output=False, unzip=True):
+        self.show_output = display_output
         self.error_descriptions = []
         self.err_files ={}
         self.root = root_path
         self.html_validator_url = 'http://validator.w3.org/check'
         self.headers = {'Content-Type': 'text/html; charset=utf-8'}
-        self.toValidate =[]
+        self.files_to_validate =[]
         self.netID = os.path.basename(root_path)
         
         if unzip == True: 
-            self.unzip(root_path)
+            self.unzip(self.root)
         
-        self.absoluteFilePaths(root_path)
+        self.getAbsFilePathsToValidate(root_path)
         self.validate()
 
     '''
-        Unzips the single .zip file in a student's directory
+        Unzips the single .zip file from this student's directory
+        root_path (String) : Absolute path to directory containing a zipped file
      '''
     def unzip(self, root_path):
         print (bcolors.OKGREEN + "\nUnzipping Website in: {}".format(self.root) + bcolors.ENDC)
+        zipped_file_count = 0
         for file in os.listdir(root_path):
             if file.endswith(".zip"):
-                zip_loc = os.path.join(root_path, file)
+                if zipped_file_count > 0 : 
+                    assert ("Too many zipped files!")
+                else:
+                    zip_loc = os.path.join(root_path, file)
+                
+                zipped_file_count += 1
         
         zip_ref = zipfile.ZipFile(zip_loc, 'r')
         zip_ref.extractall(root_path)
         zip_ref.close()
 
-        # don't know why this seems to happen, remove excess directories for macOS
+        # Don't know why this seems to happen, remove __MACOSX directory for macOS
         if "Darwin" in platform.platform():
             rmv_dir = root_path
             if "__MACOSX" in os.listdir(rmv_dir):
@@ -52,10 +70,12 @@ class Individual_Validate():
                 shutil.rmtree(rmv_macOS)
         
         
-    '''Controller for the validation'''   
+    '''
+        Controller for the validation, passes each students unzipped directory to the validator
+    '''   
     def validate(self):
         print (bcolors.OKGREEN + "\nValidating Student:{}".format(self.root) + bcolors.ENDC)
-        for file in self.toValidate:
+        for file in self.files_to_validate:
             self.runValidation(file)
             time.sleep(0.5)
 
@@ -63,12 +83,14 @@ class Individual_Validate():
         if len(self.error_descriptions) == 0:
             print ("\n\t" + bcolors.OKGREEN + "No errors found!" + bcolors.ENDC)
         
-    ''' Actually validates this students particular files'''
-    def runValidation(self, fullFilePath):
-        print ("\n\tExamining file: {}".format(fullFilePath))
-        
+    '''
+        Validates an individual file through the w3 validation service
+        path_to_file (String) : Absolute path to a students file
+    '''
+    def runValidation(self, path_to_file):
+        print ("\n\tExamining file: {}".format(path_to_file))
         # Read in raw html 
-        data = open(fullFilePath , 'rb').read()
+        data = open(path_to_file , 'rb').read()
         # Send to validator, output comes back as JSON
         output = requests.post('https://validator.w3.org/nu/?out=json', headers=self.headers, data=data).json()
         
@@ -79,30 +101,32 @@ class Individual_Validate():
                 # If a valid HTML error, print it out
                 if entry["type"] == "error":
                     self.error_descriptions.append(entry["message"])
-                    # self.err_files.append(os.path.basename(fullFilePath))
+
                     if self.netID in self.err_files:
-                        self.err_files[self.netID].add(os.path.basename(fullFilePath))
+                        self.err_files[self.netID].add(os.path.basename(path_to_file))
                     else:
-                        self.err_files[self.netID] = {os.path.basename(fullFilePath)}
+                        self.err_files[self.netID] = {os.path.basename(path_to_file)}
 
                     print ("\t\t" + bcolors.FAIL + "ERROR FOUND!" + bcolors.ENDC)
                     print ("\t\t" + entry["message"])
         
-        # If you want to see the raw JSON output for each file, pass a 'True' into the class initializer, 
+        # If you want to see the raw JSON output for each file, pass a 'True' into display_output, 
         # This is NOT enabled by default
         if self.show_output: 
             print ("Displaying file's output...")
             print("\t\t+"+json.dumps(output, indent=4, sort_keys=True))
         
-    '''recurses through directory and gets .html or .php files for validation'''
-    def absoluteFilePaths(self,directory):
+    '''
+        Recurses through directory and gets .html or .php files for validation
+        directory (String) : Absolute path to a student's unzipped directory
+    '''
+    def getAbsFilePathsToValidate(self,directory):
         for dirpath,_,filenames in os.walk(directory):
             for f in filenames:
                 if f.endswith(".html") or f.endswith(".php"):
-                    self.toValidate.append(os.path.abspath(os.path.join(dirpath, f)))
-                    # yield os.path.abspath(os.path.join(dirpath, f))
+                    self.files_to_validate.append(os.path.abspath(os.path.join(dirpath, f)))
                 
 if __name__ == '__main__':
     if len (sys.argv) >= 2:
-        Individual_Validate(sys.argv[1], displayOutput=False, )  
+        Individual_Validate(sys.argv[1], display_output=False)  
         
--- organize_grading.py
+++ organize_grading.py
@@ -2,18 +2,28 @@ import os
 import sys
 from colors import bcolors
 
-"""
+'''
     This is used for generating a grading template from Kyle's markdown template and the CMS directory output of all the students 
     a grader needs to grade for that week. It generates a file with the netID of the student at the top, and the contents of the
     rubric for each student in 1 big file with a name of your choosing
-"""
+
+    self.
+        netIDs (List[String]: None) : All netID's this grader has
+        root (String) : Absolute path to the CMS output directory
+        file_template (String) : Absolute path to location of Kyle's markup template
+        grading_file_name (String) : Name of response file (with extension)
+        grading_file_full_path (String) : CMSdirectory + grading_file_name
+        actual_grading_rubric (List[String]:None) : List containing each line of response file
+
+'''
 
 class createGradingPage():
     
     def __init__(self, root_path, file_template, grading_file_name):
-        self.netIDs = []
+        self.netIDs = None
         self.root = root_path
         self.file_template = file_template
+        self.grading_file_name = grading_file_name
         self.grading_file_full_path = os.path.join(self.root, grading_file_name)
         self.actual_grading_rubric = None
 
@@ -25,16 +35,19 @@ class createGradingPage():
     def setupWriteData(self, destination):
         template = open(self.file_template, "r")
         lines = template.readlines()
-        # Find grading template within the entire template
+        # Find grading template within the raw template
         for index in range(len(lines)):
+            #assuming this is a good way to find out where the actual feedback starts
             if "====================8<----------------------" in lines[index]:
                 self.actual_grading_rubric = lines[index+1:]
                 return 
 
     # Get the netIDs for each of the graders students        
     def getNetIDS(self, root_path):
+        netIDs = []
         for dir in os.listdir(root_path):
-            self.netIDs.append(dir)
+            netIDs.append(dir)
+        self.netIDs = netIDs
     
     # Actually writes the grading file 
     def generateGradingFile(self):
